65e82b26cc27ee9d8b54e7fd48b9bcdda54b722c
Gianluca Formica <gianluca.formica@gmail.com>
2022-05-20 13:30:57 +0100

    dzvf

--- play.js
+++ play.js
@@ -1,4 +1,15 @@
-var name="Max";
-var age=29;
-var hasHobbies=true;
-console.log(name, age, hasHobbies);
+var name = 'Max';
+var age = 29;
+var hasHobbies = true;
+//console.log(name, age, hasHobbies);
+
+function summarizeUser(userName, userAge, userHasHobby){
+	return (
+		'Name is ' + userName +
+		', age is ' + userAge +
+		' and the user has hobbies: ' +
+		userHasHobby
+	);
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
